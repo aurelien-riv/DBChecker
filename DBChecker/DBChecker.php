44660f139164a77f21e57d310739a9b2ded8c96a
Aurélien Rivière <aurelien.riv@gmail.com>
2018-04-03 12:40:16 +0200

    fix last commit mistake

--- DBChecker/DBChecker.php
+++ DBChecker/DBChecker.php
@@ -20,24 +20,24 @@ class DBChecker
 
     public function run()
     {
-//        $check = new RelCheck($this->config);
-//        foreach ($check->run() as $msg)
-//            yield $msg;
+        $check = new RelCheck($this->config);
+        foreach ($check->run() as $msg)
+            yield $msg;
 
         $check = new FileCheck($this->config);
         foreach ($check->run() as $msg)
             yield $msg;
 
-//        $check = new DataIntegrityCheck($this->config);
-//        foreach ($check->run() as $msg)
-//            yield $msg;
-//
-//        $check = new SchemaIntegrityCheck($this->config);
-//        foreach ($check->run() as $msg)
-//            yield $msg;
-//
-//        $check = new UniqueIntegrity($this->config);
-//        foreach ($check->run() as $msg)
-//            yield $msg;
+        $check = new DataIntegrityCheck($this->config);
+        foreach ($check->run() as $msg)
+            yield $msg;
+
+        $check = new SchemaIntegrityCheck($this->config);
+        foreach ($check->run() as $msg)
+            yield $msg;
+
+        $check = new UniqueIntegrity($this->config);
+        foreach ($check->run() as $msg)
+            yield $msg;
     }
 }
